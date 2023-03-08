b019cb697ec05c8105bd13a053558571a2a3a2dd
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2023-03-08 10:07:25 +0100

    Try with touching gc.properties

--- src/scripts/prepare_cleanup.sh
+++ src/scripts/prepare_cleanup.sh
@@ -10,21 +10,8 @@ fi
 echo "Folder sizes:"
 du -h --max-depth=1 ~/.gradle/caches
 if cmp -s "/tmp/git_last_previous_first_hash" "/tmp/git_last_hash" ; then
-  echo "No new cache entry will be created, build files did not change."
-  GRADLE_PROPERTIES="$GRADLE_DIRECTORY/gradle.properties"
-  echo "org.gradle.cache.cleanup=false" >> $GRADLE_PROPERTIES
-else
-  if [ -z "$PARAM_APP_DIRECTORY" ] ; then
-    PARAM_APP_DIRECTORY="."
-  fi
-
-  gradleWrapperMainVersion="$(cat $PARAM_APP_DIRECTORY/gradle/wrapper/gradle-wrapper.properties | grep distributionUrl | cut -d'-' -f 2 | cut -d'.' -f 1)"
-  if [ "$gradleWrapperMainVersion" -ge "8" ]; then
-    # for gradle 8+ - https://docs.gradle.org/current/userguide/init_scripts.html#sec:using_an_init_script
-    GRADLE_INIT_DIRECTORY="$GRADLE_DIRECTORY/init.d"
-    if [[ ! -e $GRADLE_INIT_DIRECTORY ]]; then
-      mkdir -p $GRADLE_INIT_DIRECTORY
-    fi
-    echo "beforeSettings { settings -> settings.caches {downloadedResources.removeUnusedEntriesAfterDays = 1}}" > $GRADLE_INIT_DIRECTORY/cleanup.gradle
-  fi
+  echo "No new cache entry will be created"
 fi
+GRADLE_PROPERTIES="$GRADLE_DIRECTORY/gradle.properties"
+echo "org.gradle.cache.cleanup=false" >> $GRADLE_PROPERTIES
+
